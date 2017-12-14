72bc155ceea6731a2d10caa06de6891d2ec6ef8a
Kurt Petrek <kurtpetrek@gmail.com>
2017-12-13 20:16:06 -0500

    adds basic Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,6 +3,60 @@ import { Link } from "react-router-dom";
 
 import styled from "styled-components";
 
+const Container = styled.header`
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+
+  nav {
+    position: absolute;
+    width: 100%;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem 5rem;
+
+    a {
+      color: white;
+      text-decoration: none;
+    }
+  }
+
+  h1 {
+    position: absolute;
+    width: 100%;
+    transform: translate(-50%, -50%);
+    top: 50%;
+    left: 50%;
+    margin: 0;
+  }
+
+  img {
+    width: 100%;
+    display: block;
+  }
+  @media (max-width: 700px) {
+    img {
+      height: 317px;
+      width: auto;
+      position: relative;
+      transform: translate(-50%, 0%);
+      top: 50%;
+      left: 50%;
+    }
+  }
+`;
+
 export default () => {
-  return <h1>Hi!</h1>;
+  return (
+    <Container>
+      <img src="./images/banner.jpg" />
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login">Log In</Link>
+      </nav>
+      <h1>Hello, welcome to Reactor</h1>
+    </Container>
+  );
 };
