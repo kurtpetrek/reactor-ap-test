139b99e84de926ad26427743d7f7a81356418820
Kurt Petrek <kurtpetrek@gmail.com>
2017-12-15 17:50:40 -0500

    creates fixed nav logic and props

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Nav = styled.nav`
+const NavContainer = styled.nav`
   position: absolute;
   width: 100%;
   top: 0;
@@ -10,7 +10,13 @@ const Nav = styled.nav`
   display: flex;
   justify-content: space-between;
   padding: 1rem 5rem;
-  transition: padding 300ms ease-out;
+  transition: padding 300ms ease-out, background 300ms ease-out;
+  z-index: 100;
+
+  &.fixed {
+    position: fixed;
+    background: rgba(30, 54, 86, 1);
+  }
 
   @media (max-width: 700px) {
     padding: 1rem 2rem;
@@ -23,11 +29,54 @@ const Nav = styled.nav`
   }
 `;
 
-export default props => {
-  return (
-    <Nav>
-      <Link to="/">Reactor</Link>
-      {props.loginHidden === true ? "" : <Link to="/login">Log In</Link>}
-    </Nav>
-  );
-};
+export default class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loginHidden: props.loginHidden,
+      fixed: false,
+      fixNav: props.fixNav || false
+    };
+  }
+
+  componentDidMount() {
+    if (window.pageYOffset > 0 && this.state.fixNav) {
+      this.setState(prevState => {
+        const state = { ...prevState };
+        state.fixed = true;
+        return state;
+      });
+    }
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = e => {
+    const pos = window.pageYOffset;
+    if (!this.state.fixed && pos > 0) {
+      this.setState(prevState => {
+        const state = { ...prevState };
+        state.fixed = true;
+        return state;
+      });
+    } else if (pos < 1) {
+      this.setState(prevState => {
+        const state = { ...prevState };
+        state.fixed = false;
+        return state;
+      });
+    }
+  };
+
+  render() {
+    return (
+      <NavContainer className={this.state.fixed && this.state.fixNav ? "fixed" : ""}>
+        <Link to="/">Reactor</Link>
+        {this.state.loginHidden === true ? "" : <Link to="/login">Log In</Link>}
+      </NavContainer>
+    );
+  }
+}
