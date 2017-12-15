f8c2ce6ea477c5145d6107039137610750596fac
Kurt Petrek <kurtpetrek@gmail.com>
2017-12-14 20:50:49 -0500

    Creates basic login logic.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,70 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import axios from "axios";
 
 import EntryInput from "./EntryInput";
 import EntryButton from "./EntryButton";
 
+const ErrorText = styled.p`
+  color: white;
+`;
+
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false,
+      error: "",
+      token: ""
+    };
+  }
+
+  handleLogin = e => {
+    e.preventDefault();
+    const formData = new FormData();
+    const form = e.target;
+    for (var i = 0; i < form.elements.length; i++) {
+      let el = form.elements[i];
+      if (el.name && el.value) {
+        formData.append(el.name, el.value);
+      }
+    }
+
+    axios({
+      method: "post",
+      url: "http://dev3.apppartner.com/Reactors/scripts/user-login.php",
+      data: formData
+    })
+      .then(res => {
+        this.setState(prevState => {
+          prevState.loggedIn = true;
+          prevState.error = "";
+          return prevState;
+        });
+        console.log(res);
+      })
+      .catch(error => {
+        console.log(error.response);
+        error = error.response;
+        this.setState(prevState => {
+          prevState.error = error.data.message;
+          return prevState;
+        });
+      });
+  };
+
   render() {
-    return (
-      <form>
-        <EntryInput type="text" name="username" placeholder="username" image="./images/ic-username.png" />
-        <EntryInput type="password" name="password" placeholder="Password" image="./images/ic-password.png" />
-        <EntryButton>Login</EntryButton>
-      </form>
-    );
+    if (!this.state.loggedIn) {
+      return (
+        <form onSubmit={this.handleLogin}>
+          <ErrorText>{this.state.error}</ErrorText>
+          <EntryInput required type="email" name="email" placeholder="Email" image="./images/ic-email.png" />
+          <EntryInput required type="password" name="password" placeholder="Password" image="./images/ic-password.png" />
+          <EntryButton type="submit">Login</EntryButton>
+        </form>
+      );
+    } else {
+      return <ErrorText>You Logged in!</ErrorText>;
+    }
   }
 }
